0252994dc7d964ccb6eeb9614311995deaf38714
jlipovetz <jlipovetz@gmail.com>
2023-11-14 12:44:52 -0600

    test stuff

--- models/testsWithCards.js
+++ models/testsWithCards.js
@@ -20,7 +20,7 @@ TestsWithCards.init(
           unique: false,
         },
     },
-    notecard_id: {
+    notecard_id1: {
         type: DataTypes.INTEGER,
         allowNull: false,
         references: {
@@ -29,6 +29,177 @@ TestsWithCards.init(
           unique: false,
         },
   },
+  notecard_id2: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: 'notecards',
+      key: 'id',
+      unique: false,
+    },
+},
+notecard_id3: {
+  type: DataTypes.INTEGER,
+  allowNull: false,
+  references: {
+    model: 'notecards',
+    key: 'id',
+    unique: false,
+  },
+},
+notecard_id4: {
+  type: DataTypes.INTEGER,
+  allowNull: false,
+  references: {
+    model: 'notecards',
+    key: 'id',
+    unique: false,
+  },
+},
+notecard_id5: {
+  type: DataTypes.INTEGER,
+  allowNull: false,
+  references: {
+    model: 'notecards',
+    key: 'id',
+    unique: false,
+  },
+},
+notecard_id6: {
+  type: DataTypes.INTEGER,
+  allowNull: false,
+  references: {
+    model: 'notecards',
+    key: 'id',
+    unique: false,
+  },
+},
+notecard_id7: {
+  type: DataTypes.INTEGER,
+  allowNull: false,
+  references: {
+    model: 'notecards',
+    key: 'id',
+    unique: false,
+  },
+},
+notecard_id8: {
+  type: DataTypes.INTEGER,
+  allowNull: false,
+  references: {
+    model: 'notecards',
+    key: 'id',
+    unique: false,
+  },
+},
+notecard_id9: {
+  type: DataTypes.INTEGER,
+  allowNull: false,
+  references: {
+    model: 'notecards',
+    key: 'id',
+    unique: false,
+  },
+},
+notecard_id10: {
+  type: DataTypes.INTEGER,
+  allowNull: false,
+  references: {
+    model: 'notecards',
+    key: 'id',
+    unique: false,
+  },
+},
+notecard_id11: {
+  type: DataTypes.INTEGER,
+  allowNull: false,
+  references: {
+    model: 'notecards',
+    key: 'id',
+    unique: false,
+  },
+},
+notecard_id12: {
+  type: DataTypes.INTEGER,
+  allowNull: false,
+  references: {
+    model: 'notecards',
+    key: 'id',
+    unique: false,
+  },
+},
+notecard_id13: {
+  type: DataTypes.INTEGER,
+  allowNull: false,
+  references: {
+    model: 'notecards',
+    key: 'id',
+    unique: false,
+  },
+},
+notecard_id14: {
+  type: DataTypes.INTEGER,
+  allowNull: false,
+  references: {
+    model: 'notecards',
+    key: 'id',
+    unique: false,
+  },
+},
+notecard_id15: {
+  type: DataTypes.INTEGER,
+  allowNull: false,
+  references: {
+    model: 'notecards',
+    key: 'id',
+    unique: false,
+  },
+},
+notecard_id16: {
+  type: DataTypes.INTEGER,
+  allowNull: false,
+  references: {
+    model: 'notecards',
+    key: 'id',
+    unique: false,
+  },
+},
+notecard_id17: {
+  type: DataTypes.INTEGER,
+  allowNull: false,
+  references: {
+    model: 'notecards',
+    key: 'id',
+    unique: false,
+  },
+},
+notecard_id18: {
+  type: DataTypes.INTEGER,
+  allowNull: false,
+  references: {
+    model: 'notecards',
+    key: 'id',
+    unique: false,
+  },
+},
+notecard_id19: {
+  type: DataTypes.INTEGER,
+  allowNull: false,
+  references: {
+    model: 'notecards',
+    key: 'id',
+    unique: false,
+  },
+},
+notecard_id20: {
+  type: DataTypes.INTEGER,
+  allowNull: false,
+  references: {
+    model: 'notecards',
+    key: 'id',
+    unique: false,
+  },
+},
   },
   {
     sequelize,
