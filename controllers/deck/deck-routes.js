8215df4287712f0a1959f2078bfd62961d803933
Osorkon21 <Osorkon21@users.noreply.github.com>
2023-11-14 12:46:48 -0600

    Begin adding deck display page

--- controllers/deck/deck-routes.js
+++ controllers/deck/deck-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
-const { Decks, Users } = require('../../models');
+const { Decks, Users, Notecards } = require('../../models');
 
 // GET one deck
 router.get('/:id', async (req, res) => {
@@ -10,9 +10,13 @@ router.get('/:id', async (req, res) => {
       include: [
         {
           model: Users,
-          attributes: ['first_name', 'last_name'],
+          attributes: ['username'],
         },
-      ],
+        {
+          model: Notecards,
+          attributes: ['question', 'answer'],
+        }
+      ]
     });
 
     var deck;
@@ -21,6 +25,42 @@ router.get('/:id', async (req, res) => {
       deck = { id: `No deck found with id ${req.params.id}` }
     else
       deck = dbDeckData.get({ plain: true });
+
+    console.log(deck);
+
+    res.render('deck-display', { deck, loggedIn: req.session.loggedIn });
+  }
+  catch (err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
+});
+
+// GET one card
+router.get('/:id', async (req, res) => {
+  try {
+    // get a Deck
+    const dbDeckData = await Decks.findByPk(req.params.id, {
+      include: [
+        {
+          model: Users,
+          attributes: ['username'],
+        },
+        {
+          model: Notecards,
+          attributes: ['question', 'answer'],
+        }
+      ]
+    });
+
+    var deck;
+
+    if (!dbDeckData)
+      deck = { id: `No deck found with id ${req.params.id}` }
+    else
+      deck = dbDeckData.get({ plain: true });
+
+    console.log(deck);
 
     res.render('deck-display', { deck, loggedIn: req.session.loggedIn });
   }
